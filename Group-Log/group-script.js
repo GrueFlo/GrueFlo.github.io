d9b6bd61c3150153fe6ad7ff470724019467a87d
GrueFlo <florian.gruenewald@stud.h-da.de>
2023-06-21 15:37:17 +0200

    updated styles

--- Group-Log/group-script.js
+++ Group-Log/group-script.js
@@ -13,19 +13,18 @@ function allowDrop(event) {
   
     const data = event.dataTransfer.getData('text/plain');
     const draggableElement = document.getElementById(data);
-    
+  
     // Check if the drop is allowed in the specified slots
     const timestamp = event.target.parentNode.querySelector('.timestamp').textContent;
-    if (
-      (timestamp === '9:00 - 12:00' || timestamp === '14:00 - 17:00') &&
-      event.target.classList.contains('droppable-column')
-    ) {
-      const originalColor = window.getComputedStyle(draggableElement).backgroundColor;
+    const isDropAllowed = timestamp === '9:00 - 12:00' || timestamp === '14:00 - 17:00';
+    const isTargetColumn = event.target.classList.contains('droppable-column') || event.target.classList.contains('backlog-column');
+  
+    if (isDropAllowed && isTargetColumn) {
       event.target.appendChild(draggableElement);
-      draggableElement.removeAttribute('draggable');
-      draggableElement.classList.remove('backlog-box');
-      draggableElement.classList.add('dropped-box');
-      draggableElement.style.backgroundColor = originalColor;
+      draggableElement.setAttribute('draggable', 'true');
+      draggableElement.classList.remove('dropped-box');
+      draggableElement.classList.add('backlog-box');
+      draggableElement.style.backgroundColor = '';
     }
   }
   
